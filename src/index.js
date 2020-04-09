81cd643d703f45b8c0019a7be01998aa2147c59c
BuildTools <nickalexeev@protonmail.com>
2020-04-09 07:48:20 -0800

    got basic engine setup

--- src/index.js
+++ src/index.js
@@ -11,14 +11,43 @@ window.addEventListener('keydown', function(event) {
     console.log(event.key);
     state.key_down_queue.push(event.key);
 });
+function draw(x,y,width,height){
+    ctx.fillStyle = 'green';
+    ctx.fillRect(x, y, width, height);
+}
+function clear(){
+    ctx.clearRect(0, 0, 300, 300);
+}
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
 const frame_time_ms = (1.0/60.0)*1000.0;
+var x=0.0;
+var y = 0.0;
+const move_speed = 0.6;
 function game_loop(){
-    console.log(state.key_down_queue);
+    clear();
+    if(state.key_down_queue.length>=1){
+        console.log(state.key_down_queue);
+    }
+    for(let i in state.key_down_queue){
+        if(state.key_down_queue[i]=="w"){
+            y+=move_speed;
+        }
+        if(state.key_down_queue[i]=="s"){
+            y-=move_speed;
+        }
+        if(state.key_down_queue[i]=="d"){
+            x+=move_speed;
+        }
+        if(state.key_down_queue[i]=="a"){
+            x-=move_speed;
+        }
+    }
+    draw(x,y,10,10);
     state.key_down_queue=[]
+    
 }
 async function main(){
     while(0==0){
