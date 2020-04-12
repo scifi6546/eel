318cd5d255a29aedc8bf0033a8a83597076477c0
BuildTools <nickalexeev@protonmail.com>
2020-04-12 12:42:06 -0800

    almost got ECS working

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate wasm_bindgen;
+#[allow(unused_imports)]
 use serde_wasm_bindgen::*;
 #[macro_use]
 extern crate serde_derive;
@@ -6,12 +7,13 @@ use wasm_bindgen::prelude::*;
 extern crate wee_alloc;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-#[derive(Serialize, Deserialize)]
+static TILE_SIZE:u32=20;
+#[derive(Serialize, Deserialize,Clone,std::cmp::PartialEq,Debug)]
 enum Tile {
     Wall,
     Floor,
 }
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize,Clone,std::cmp::PartialEq,Debug)]
 pub struct Vector2 {
     x: i32,
     y: i32,
@@ -46,7 +48,7 @@ impl Tile {
         }
     }
 }
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize,Clone)]
 struct Grid {
     tiles: Vec<Tile>,
     width: u32,
@@ -61,16 +63,23 @@ impl Grid {
             height: height,
         }
     }
+    pub fn get_tile(&self, position:Vector2)->Option<Tile>{
+        let index = (position.x as u32 * self.width + position.y as u32) as usize;
+        if index<self.tiles.len(){
+            return Some(self.tiles[index].clone());
+        }
+        None
+    }
     pub fn draw(&self) -> Vec<u32> {
         let mut out: Vec<u32> = vec![];
         for x in 0..self.width {
             for y in 0..self.height {
                 out.append(&mut vec![
                     self.tiles[(x * self.width + y) as usize].get_color(),
-                    x * 10,
-                    y * 10,
-                    10,
-                    10,
+                    x * TILE_SIZE,
+                    y * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE,
                 ])
             }
         }
@@ -78,45 +87,95 @@ impl Grid {
     }
 }
 #[derive(Serialize, Deserialize)]
-struct Player {
-    pos:Vector2,
-    health: u32,
-}
-impl Player{
-    pub fn draw(&self)->Vec<u32>{
-        vec![0x00ff00,(self.pos.x*10) as u32 ,(self.pos.y*10) as u32,10,10]
-    }
-}
-#[derive(Serialize, Deserialize)]
-struct Enemy {
-    pos:Vector2,
-    health: u32,
-}
-
-#[derive(Serialize, Deserialize)]
 pub struct State {
     entities:Vec<Entity>,
-
     grid: Grid,
+    input:InputComponent,
+    grid_component:GridComponent,
+}
+impl State{
+    pub fn process(&mut self, input:Vector2){
+        let mut entities_v = vec![];
+        for ent in self.entities.iter(){
+            let test = self.input.apply(ent.clone(),self.grid.clone(),input.clone());
+            entities_v.push(test.0);
+            self.grid=test.1;
+        }
+        self.entities=entities_v;
+        entities_v = vec![];
+        for ent in self.entities.iter(){
+            let test = self.grid_component.apply(ent.clone(),self.grid.clone(),input.clone());
+            entities_v.push(test.0);
+            self.grid=test.1;
+        }
+        self.entities=entities_v;
+    }
+    #[allow(dead_code)]
+    fn get_tile(&self,position:Vector2)->Option<Tile>{
+        return self.grid.get_tile(position)
+    }
+    pub fn draw(&self)->Vec<u32>{
+        let mut draws = self.grid.draw();
+        for ent in self.entities.iter(){
+            draws.append(&mut vec![0x00ff00,(ent.position.x as u32 * TILE_SIZE) as u32,(ent.position.y as u32 * TILE_SIZE) as u32,TILE_SIZE,TILE_SIZE]);
+        }
+        return draws;
+    }
+    #[allow(dead_code)]
+    fn get_entities(&self)->&Vec<Entity>{
+        &self.entities
+    }
 }
 #[derive(Serialize)]
 pub struct MainOutput {
     pub state: State,
     pub draw_calls: Vec<u32>,
 }
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize,Clone)]
 struct Entity{
-    position:Vector2
+    position:Vector2,
+    delta_position:Vector2,
+}
+impl Entity{
+    pub fn new(pos:Vector2)->Entity{
+        Entity{
+            position:pos,
+            delta_position:Vector2::new(0,0),
+        }
+    }
 }
 trait Component{
-    fn apply(entity: &mut std::rc::Rc<std::cell::RefCell<Entity>>,world: &mut Grid,input:Vector2);
+    fn apply(&self,entity: Entity,world: Grid,input:Vector2)->(Entity,Grid);
 }
+
+#[derive(Serialize, Deserialize)]
 struct InputComponent{
 
 }
 impl Component for InputComponent{
-    fn apply(entity: &mut std::rc::Rc<std::cell::RefCell<Entity>>,world: &mut Grid,input:Vector2){
-        entity.borrow_mut().position+=input;
+    fn apply(&self,entity: Entity,world: Grid,input:Vector2)->(Entity,Grid){
+        let mut entity_n = entity;
+        entity_n.delta_position=input;
+        return (entity_n,world);
+    }
+}
+#[derive(Serialize, Deserialize)]
+struct GridComponent{
+
+}
+impl Component for GridComponent{
+    fn apply(&self,entity: Entity,world: Grid,_input:Vector2)->(Entity,Grid){
+        if let Some(tile) = world.get_tile(entity.position.clone()+entity.delta_position.clone()){
+            let mut entity_m = entity;
+            if tile!=Tile::Wall{
+                entity_m.position+=entity_m.delta_position;
+            }
+            entity_m.delta_position=Vector2::new(0, 0);
+            return (entity_m,world)
+        }
+        let mut entity_m = entity;
+        entity_m.delta_position=Vector2::new(0, 0);
+        return (entity_m,world);
     }
 }
 #[wasm_bindgen]
@@ -130,22 +189,19 @@ pub fn game_loop_js(input: JsValue, state_in: JsValue) -> JsValue {
 }
 
 pub fn game_loop(input:Vector2,state: State) -> MainOutput {
+
     let mut state_m = state;
-    let mut input_m = input;
-    input_m.y*=-1;
-    state_m.player.pos+=input_m;
-    let mut draw_calls = state_m.grid.draw();
-    draw_calls.append(&mut state_m.player.draw());
+    state_m.process(input);
 
     MainOutput {
-        draw_calls: draw_calls,
+        draw_calls: state_m.draw(),
         state: state_m,
     }
 }
 
 pub fn init_state() -> State {
     State {
-        entities:vec![],
+        entities:vec![Entity::new(Vector2::new(1, 1))],
         grid: Grid::new(
             5,
             5,
@@ -177,6 +233,8 @@ pub fn init_state() -> State {
                 Tile::Wall,
             ],
         ),
+        input:InputComponent{},
+        grid_component:GridComponent{}
     }
 }
 #[wasm_bindgen]
@@ -218,6 +276,14 @@ mod tests {
         assert_eq!(v1.x,1);
         assert_eq!(v1.y,1);
 
+    }
+    #[test]
+    fn run_frame_input(){
+        let mut s = init_state();
+        s = game_loop(Vector2::new(1, 0),s).state;
+        assert_eq!(s.get_tile(Vector2::new(2,1)),Some(Tile::Floor));
+        let e = &s.get_entities()[0];
+        assert_eq!(e.position,Vector2::new(2, 1));
     }
 
 
